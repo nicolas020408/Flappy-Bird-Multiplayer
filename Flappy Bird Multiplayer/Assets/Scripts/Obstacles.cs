6953347fed8c7d0f6057ca4c0364d8ad16e2ea22
zaikodan <zaikodan@gmail.com>
2024-08-20 09:12:20 -0300

    Update Obstacles.cs

--- Flappy Bird Multiplayer/Assets/Scripts/Obstacles.cs
+++ Flappy Bird Multiplayer/Assets/Scripts/Obstacles.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float speed = 3.5f;
+
+    Rigidbody2D rigidbody2D;
+
+    private void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        /*
+        if(transform.position.x < -GameManager.instance.ScreenBounds.x)
+        {
+            Destroy(gameObject);
+        }*/
+
+        rigidbody2D.velocity = Vector2.left * speed;
     }
 }
