50224c7e10dae5b979b87b00f77aea75fcb96b2f
zaikodan <zaikodan@gmail.com>
2024-08-21 13:44:14 -0300

    Create Player

--- Flappy Bird Multiplayer/Assets/Scripts/GameManager.cs
+++ Flappy Bird Multiplayer/Assets/Scripts/GameManager.cs
@@ -1,8 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPun
 {
     #region Singleton
     public static GameManager instance;
@@ -27,4 +28,28 @@ public class GameManager : MonoBehaviour
 
     public Vector2 ScreenBounds { get => screenBounds; }
     public int Score { get => score; set => score = value; }
+
+    const string playerPrefabPath = "Prefabs/Player";
+    int playersInGame = 0;
+
+    private void Start()
+    {
+        photonView.RPC("AddPlayer", RpcTarget.AllBuffered);
+    }
+
+    private void CreatePlayer()
+    {
+        Player player = NetworkManager.instance.Instantiate(playerPrefabPath, new Vector3(30, 1, 30), Quaternion.identity).GetComponent<Player>();
+        player.photonView.RPC("Initialize", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void AddPlayer()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            CreatePlayer();
+        }
+    }
 }
--- Flappy Bird Multiplayer/Assets/Scripts/Player.cs
+++ Flappy Bird Multiplayer/Assets/Scripts/Player.cs
@@ -1,17 +1,30 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPun
 {
     const float jumpForce = 8;
     Rigidbody2D rigidbody2D;
     UIManager managerUI;
 
+    bool controllerOn;
+
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         managerUI = FindObjectOfType<UIManager>();
+    }
+
+    [PunRPC]
+    private void Initialize()
+    {
+        if (!photonView.IsMine)
+        {
+            
+            controllerOn = false;
+        }
     }
 
     private void Update()
